8f7129abcdc8932520383b2f696635c79f76e5a7
Remington Campbell <code@remington.io>
2019-02-25 22:26:14 -0800

    Consolidate into dockerized network

---
+++ yggdrassil/plex.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PLEX_TZ=America/Los_Angeles
+PLEX_CLAIM=
+
+docker run \
+    -d \
+    --name plex \
+    --network=host \
+    -e TZ="${PLEX_TZ}" \
+    -e PLEX_CLAIM="${PLEX_CLAIM}" \
+    -v /datalake/plex/database:/config \
+    -v /datalake/plex/transcode:/transcode \
+    -v /datalake/plex/media:/data \
+    plexinc/pms-docker:plexpass
